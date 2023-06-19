225394d9288ca6875a6cf855546c7a6f15fbd71f
patshikwe <patshikwe@yahoo.fr>
2023-06-19 04:23:26 +0200

    Select background color

---
+++ script.js
@@ -0,0 +1,25 @@
+const container = document.getElementById("container");
+const btns = document.getElementsByClassName('btn');
+const paragraph = document.createElement("p");
+container.appendChild(paragraph);
+
+for (const btn of btns) {
+    btn.addEventListener('click', function () {
+        const buttonColor = getComputedStyle(this);
+        const buttunBackgroundColor = buttonColor["backgroundColor"];
+        document.body.style.backgroundColor = buttunBackgroundColor;
+        const btnNumber = btn.innerHTML;
+        paragraph.innerHTML = `This is button number ${btnNumber}.`;
+        paragraph.style.textAlign = 'center';
+        paragraph.style.fontSize = "20px";
+        paragraph.style.marginTop = "10px";
+
+        if (btnNumber === '1' || btnNumber === '2' || btnNumber === '3') {
+            paragraph.style.color = "white";
+        }
+
+        if (btnNumber === '4' || btnNumber === '5' || btnNumber === '6') {
+            paragraph.style.color = "black";
+        }
+    })
+}
